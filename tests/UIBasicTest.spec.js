d20a63e67f55365ab7e283bed86f7b79e61c8a44
markamcfadden <mcfadden.a.mark@gmail.com>
2025-03-27 12:33:55 -0300

    add tests for sign in

--- tests/UIBasicTest.spec.js
+++ tests/UIBasicTest.spec.js
@@ -1,9 +1,28 @@
 const { test, expect } = require("@playwright/test");
 
-test("First Playwright Test", async ({ browser }) => {
+test.only("First Playwright Test", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
+
+  const userName = page.locator("input#username");
+  const password = page.locator("input#password");
+  const signInBtn = page.locator("input#signInBtn");
+  const cardTitles = page.locator(".card-body a");
+
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  console.log(await page.title());
+  await userName.fill("rahulshetty");
+  await password.fill("learning");
+  await signInBtn.click();
+  console.log(await page.locator("[style*='block']").textContent(""));
+  await expect(page.locator("[style*='block']")).toContainText("Incorrect");
+
+  await userName.fill("");
+  await userName.fill("rahulshettyacademy");
+  await signInBtn.click();
+  //console.log(await cardTitles.nth(0).textContent());
+  const allTitles = await cardTitles.allTextContents();
+  console.log(allTitles);
 });
 
 test("Page Playwright Test", async ({ page }) => {

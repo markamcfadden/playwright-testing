4dcbb14b99fe77922222c1c8aa602ffaf2a249f7
markamcfadden <mcfadden.a.mark@gmail.com>
2025-03-27 12:33:29 -0300

    add new wait times

--- playwright.config.js
+++ playwright.config.js
@@ -1,14 +1,14 @@
 // @ts-check
-import { defineConfig, devices } from "@playwright/test";
+import { defineConfig } from "@playwright/test";
 
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
   testDir: "./tests",
-  timeout: 40 * 1000,
+  timeout: 30 * 1000,
   expect: {
-    timeout: 40 * 1000,
+    timeout: 5000,
   },
   reporter: "html",
   use: {

2db3fb43d2ebdee166ab97468c980251a676d701
markamcfadden <mcfadden.a.mark@gmail.com>
2025-03-27 14:46:09 -0300

    separate test files

---
+++ tests/ClientApp.spec.js
@@ -0,0 +1,19 @@
+const { test, expect } = require("@playwright/test");
+
+test.only("broswer context validating login", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/client/");
+
+  const email = page.locator("input#userEmail");
+  const password = page.locator("input#userPassword");
+  const loginBtn = page.locator("input#login");
+
+  await email.fill("auto_tests_account@proton.me");
+  await password.fill("Testing4455$");
+  await loginBtn.click();
+
+  //await page.waitForLoadState("networkidle"); a little flaky
+  await page.locator(".card-body b").first().waitFor();
+
+  const titles = await page.locator(".card-body b").allTextContents();
+  console.log(titles);
+});
--- tests/UIBasicTest.spec.js
+++ tests/UIBasicTest.spec.js
@@ -25,25 +25,6 @@ test("First Playwright Test", async ({ browser }) => {
   console.log(allTitles);
 });
 
-test.only("E Commerce Sign In", async ({ page }) => {
-  await page.goto("https://rahulshettyacademy.com/client/");
-
-  const email = page.locator("input#userEmail");
-  const password = page.locator("input#userPassword");
-  const loginBtn = page.locator("input#login");
-
-  await email.fill("auto_tests_account@proton.me");
-  await password.fill("Testing4455$");
-  await loginBtn.click();
-
-  const firstCardTitle = await page
-    .locator(".card-body b")
-    .nth(0)
-    .textContent();
-
-  await expect(firstCardTitle).toBe("ZARA COAT 3");
-});
-
 test("Page Playwright Test", async ({ page }) => {
   await page.goto("https://google.com");
   //get title - assertion

4290fb9121f96164919cf44bcf274b417934a40d
markamcfadden <mcfadden.a.mark@gmail.com>
2025-03-31 18:39:20 -0300

    add getbylabels

--- tests/llc.spec.js
+++ tests/llc.spec.js
@@ -2,4 +2,8 @@ import { test, expect } from "@playwright/test";
 
 test("Playwright Special locators", async ({ page }) => {
   await page.goto("https://rahulshettyacademy.com/angularpractice/");
+
+  await page.getByLabel("Check me out if you Love IceCreams!").click();
+  await page.getByLabel("Enployed").check();
+  await page.getByLabel("Gender").selectOption("Male");
 });

528d7f5a207f372e8c5f7ff3362b6fbeebf351a1
markamcfadden <mcfadden.a.mark@gmail.com>
2025-03-31 18:32:32 -0300

    add test file for special locators

---
+++ tests/llc.spec.js
@@ -0,0 +1,5 @@
+import { test, expect } from "@playwright/test";
+
+test("Playwright Special locators", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/angularpractice/");
+});
